98c2b526964a37b0ad205383fe5b363b282bcbd6
Marco Nie <github@niekun.net>
2021-03-27 21:26:30 +0800

    update

--- v2ray/openwrt/v2ray.sh
+++ v2ray/openwrt/v2ray.sh
@@ -1,8 +1,8 @@
 #!/bin/sh /etc/rc.common
 START=100
 start() {
-    /usr/bin/v2ray -confdir /etc/v2ray/conf.d &
-    /sbin/ip rule add fwmark 1 table 100
-    /sbin/ip route add local default dev lo table 100
-    /usr/sbin/iptables-restore < /etc/iptables/v2ray-rules
+    v2ray -confdir /etc/v2ray/conf.d &
+    ip rule add fwmark 1 table 100
+    ip route add local default dev lo table 100
+    iptables-restore < /etc/v2ray/iptables
 }
