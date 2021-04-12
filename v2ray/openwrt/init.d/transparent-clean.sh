57aaa3c8eada4450f5e9cd88f5af54d6688c9381
Marco Nie <github@niekun.net>
2021-04-12 10:35:07 +0800

    update

---
+++ v2ray/openwrt/init.d/transparent-clean.sh
@@ -0,0 +1,15 @@
+ip route del local default dev lo table 100
+ip rule del fwmark 1 lookup 100
+
+ipset -X gfwlist
+ipset -X gfwlist_ext
+
+iptables -t mangle -D PREROUTING -p tcp -m set --match-set gfwlist dst -j TPROXY --on-port 1081 --tproxy-mark 1
+iptables -t mangle -D PREROUTING -p udp -m set --match-set gfwlist dst -j TPROXY --on-port 1081 --tproxy-mark 1
+iptables -t mangle -D OUTPUT -p tcp -m set --match-set gfwlist dst -j MARK --set-mark 1
+iptables -t mangle -D OUTPUT -p udp -m set --match-set gfwlist dst -j MARK --set-mark 1
+
+iptables -t mangle -D PREROUTING -p tcp -m set --match-set gfwlist_ext dst -j TPROXY --on-port 1081 --tproxy-mark 1
+iptables -t mangle -D PREROUTING -p udp -m set --match-set gfwlist_ext dst -j TPROXY --on-port 1081 --tproxy-mark 1
+iptables -t mangle -D OUTPUT -p tcp -m set --match-set gfwlist_ext dst -j MARK --set-mark 1
+iptables -t mangle -D OUTPUT -p udp -m set --match-set gfwlist_ext dst -j MARK --set-mark 1
