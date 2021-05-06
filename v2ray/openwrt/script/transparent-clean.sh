149ea6a1d7407fa4c94fa05f1c55b3563fe22b3e
Marco Nie <github@niekun.net>
2021-05-06 22:03:49 +0800

    update

--- v2ray/openwrt/script/transparent-clean.sh
+++ v2ray/openwrt/script/transparent-clean.sh
@@ -13,6 +13,3 @@ iptables -t mangle -D PREROUTING -p tcp -m set --match-set gfwlist_ext dst -j TP
 iptables -t mangle -D PREROUTING -p udp -m set --match-set gfwlist_ext dst -j TPROXY --on-port 1081 --tproxy-mark 1
 iptables -t mangle -D OUTPUT -p tcp -m set --match-set gfwlist_ext dst -j MARK --set-mark 1
 iptables -t mangle -D OUTPUT -p udp -m set --match-set gfwlist_ext dst -j MARK --set-mark 1
-
-iptables -t mangle -D PREROUTING -d 198.18.0.0/15 -j TPROXY --on-port 1081 --tproxy-mark 1
-iptables -t mangle -D OUTPUT -d 198.18.0.0/15 -j MARK --set-mark 1
