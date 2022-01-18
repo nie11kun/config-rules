128da1e1fbe393ddca3bebc4f6a1ef60c7f1829c
Marco Nie <github@niekun.net>
2022-01-18 19:25:11 +0800

    update

---
+++ Shadowrocket/get_jdcookie.js
@@ -0,0 +1,30 @@
+/*************************
+初次使用时, app配置文件添加脚本配置, 并启用Mitm后:
+Safari浏览器打开登录 https://home.m.jd.com/myJd/newhome.action 点击"我的"页面
+
+[mitm]
+hostname = api.m.jd.com
+*************************/
+
+var $nobyda = nobyda();
+
+GetCookie();
+
+function GetCookie() {
+    const req = $request;
+    if (req.method != 'OPTIONS' && req.headers) {
+        const CV = (req.headers['Cookie'] || req.headers['cookie'] || '');
+        const ckItems = CV.match(/(pt_key|pt_pin)=.+?;/g);
+        $nobyda.notify(`cookie`, `successful get jd cookie`, `${ckItems}`);
+    } else if (!req.headers) {
+        throw new Error("error found");
+    }
+}
+
+function nobyda() {
+    const notify = (title, subtitle, message) => {
+        $notification.post(title, subtitle, message, undefined)
+    }
+
+    return notify;
+};
