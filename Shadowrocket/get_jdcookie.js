a79b1d7e370d73c0a9639f697ef674881f931961
Marco Nie <github@niekun.net>
2022-01-18 19:45:42 +0800

    update

--- Shadowrocket/get_jdcookie.js
+++ Shadowrocket/get_jdcookie.js
@@ -2,6 +2,9 @@
 初次使用时, app配置文件添加脚本配置, 并启用Mitm后:
 Safari浏览器打开登录 https://home.m.jd.com/myJd/newhome.action 点击"我的"页面
 
+[Script]
+get_jdcookie = type=http-request,script-path=https://raw.githubusercontent.com/nie11kun/config-rules/master/Shadowrocket/get_jdcookie.js,pattern=^https?://api\.m\.jd\.com/client\.action\?functionId=GetJDUserInfoUnion,max-size=131072,timeout=10,enable=true
+
 [mitm]
 hostname = api.m.jd.com
 *************************/
