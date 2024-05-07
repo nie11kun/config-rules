b8d797ec85f68c79ef064ef63d5e5b3adc81be2c
Marco Nie <github@niekun.net>
2024-05-07 09:53:46 +0800

    dnsmasq mode working

--- v2ray/V5/client/openwrt/script/dnsmasq/transparent.sh
+++ v2ray/V5/client/openwrt/script/dnsmasq/transparent.sh
@@ -1,27 +1,17 @@
-# 配合 dnsmasq 对需要代理的域名和ip地址走 tproxy 的 iptables 规则
-# 可以避免全局透明代理时 v2ray router 中基于域名的规则都无效的问题
-# 使用此模式可以精简 v2ray config 配置，只需要保留进入的流量都走 vmess 出口即可 dns 只保留 1.1.1.1 即可 防止域名污染
+# 配合 dnsmasq 对需要代理的域名和ip地址走 redirect 的 nftables 规则 需要安装 dnsmasq-full
+# v2ray dokodemo-door 需要设置 "tproxy": "redirect"
+# 只能接收局域网设备流量 本机网关流量不转发
 
-ip route add local default dev lo table 100
-ip rule add fwmark 1 table 100
+# 新建路由表
+nft add table v2ray
 
-ipset -X gfwlist
-ipset create gfwlist hash:ip
+# 添加 set 表
+nft add set ip v2ray gfwlist { type ipv4_addr\; }
 
-ipset -X gfwlist_ext
-ipset create gfwlist_ext hash:net
+# 读取读取文件中的 ip 地址到 gfwlist 表中
 for ip in $(cat /etc/dnsmasq/proxy_ip.txt);
-    do ipset add gfwlist_ext $ip;
+    do nft add element ip v2ray gfwlist { $ip };
 done
 
-iptables -t mangle -A OUTPUT -j RETURN -m mark --mark 0x02 #防止v2ray处理过的流量回环 需放在第一句最先判断 v2ray outbound 中需要设置 mark 2
-
-iptables -t mangle -A PREROUTING -p tcp -m set --match-set gfwlist dst -j TPROXY --on-port 1081 --tproxy-mark 1
-iptables -t mangle -A PREROUTING -p udp -m set --match-set gfwlist dst -j TPROXY --on-port 1081 --tproxy-mark 1
-iptables -t mangle -A OUTPUT -p tcp -m set --match-set gfwlist dst -j MARK --set-mark 1
-iptables -t mangle -A OUTPUT -p udp -m set --match-set gfwlist dst -j MARK --set-mark 1
-
-iptables -t mangle -A PREROUTING -p tcp -m set --match-set gfwlist_ext dst -j TPROXY --on-port 1081 --tproxy-mark 1
-iptables -t mangle -A PREROUTING -p udp -m set --match-set gfwlist_ext dst -j TPROXY --on-port 1081 --tproxy-mark 1
-iptables -t mangle -A OUTPUT -p tcp -m set --match-set gfwlist_ext dst -j MARK --set-mark 1
-iptables -t mangle -A OUTPUT -p udp -m set --match-set gfwlist_ext dst -j MARK --set-mark 1
+nft add chain v2ray prerouting { type nat hook prerouting priority 0 \; policy accept \; }
+nft add rule v2ray prerouting ip daddr @gfwlist ip protocol tcp redirect to :1081
